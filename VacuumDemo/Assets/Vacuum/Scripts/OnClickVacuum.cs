ecfe89b12e61417b03c1a5caded7d91ef68ed495
akj1 <kellerr1@abv.bg>
2016-09-18 13:53:30 +0300

    Final Touches

--- VacuumDemo/Assets/Vacuum/Scripts/OnClickVacuum.cs
+++ VacuumDemo/Assets/Vacuum/Scripts/OnClickVacuum.cs
@@ -4,15 +4,13 @@ namespace Assets.Vacuum.Scripts
 {
     public class OnClickVacuum : MonoBehaviour
     {
-        public Vector2 LockedPosition = Vector3.zero;
         public void Update()
         {
            if (Input.GetMouseButtonDown(0))
            {
                 transform.GetComponent<Animator>().SetTrigger("Triggered");
            }
-
-            transform.position = Camera.main.ScreenPointToRay(Input.mousePosition).GetPoint(9);
+           transform.position = Camera.main.ScreenPointToRay(Input.mousePosition).GetPoint(9);
         }
     }
 }
